2a04d74fba3a016d4cfb6b55cece311f5a6e0daf
MS_D_rei <msmdmission1111@gmail.com>
2023-10-31 21:02:33 +0900

    Add Chapter 6 contents

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is my note for [course curriculum](https://nextjs.org/learn) on the Next.js Website.
 
-## Chapter 3
+## Chapter 3 Optimizing Fonts and Images
 
 ### 1. Why optimize font?
 This is for Cumulative Layout Shift (CLS) problem
@@ -24,7 +24,7 @@ We can use <img>. However, we have to manually do the following things.
 - Lazy load images that are outside the user's viewpoint.
 NextJS Image resolves these issues.
 
-## Chapter 4, 5
+## Chapter 4, 5 Layouts, pages and Navigation
 
 How Routing and Navigation works
 Ref: https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#how-routing-and-navigation-works
@@ -53,3 +53,86 @@ remain scroll position, focus element by default.
 ### 5. Back and Forward Navigation
 I think this is included in soft navigation explanation.
 
+## Chapter 6 Setting Up Database
+I got error below
+
+```
+Created "invoices" table
+Error seeding invoices: NeonDbError: db error: ERROR: prepared statement "s6649510" does not exist
+
+Caused by:
+    ERROR: prepared statement "s6649510" does not exist
+    at execute (/Users/nextjs-dashboard/node_modules/@neondatabase/serverless/index.js:1539:48)
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at async Promise.all (index 0)
+    at async seedInvoices (/Users/nextjs-dashboard/scripts/seed.js:67:30)
+    at async /Users/nextjs-dashboard/scripts/seed.js:166:3 {
+  code: '26000',
+  sourceError: undefined
+}
+node:internal/process/promises:288
+            triggerUncaughtException(err, true /* fromPromise */);
+            ^
+
+```
+Solution: https://github.com/vercel/next-learn/pull/314
+```js
+// seed.js
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+
+    console.log(`Created "users" table`);
+
+    // Insert data into the "users" table
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+        `;
+      }),
+    );
+
+    console.log(`Seeded ${insertedUsers.length} users`);
+
+    return {
+      createTable,
+      users: insertedUsers,
+    };
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    throw error;
+  }
+}
+
+// abbriviate seedInvoices, seedCustomers, seedRevenue
+
+async function main() {
+  const client = await db.connect();
+
+  await seedUsers(client);
+  await seedInvoices(client);
+  await seedCustomers(client);
+  await seedRevenue(client);
+
+  await client.end();
+}
+
+main().catch((error) => {
+  console.error("Error occurred while seeding database:", error);
+});
+```
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,16 +1,16 @@
-const { sql } = require('@vercel/postgres');
+const { sql } = require("@vercel/postgres");
 const {
   invoices,
   customers,
   revenue,
   users,
-} = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
+} = require("../app/lib/placeholder-data.js");
+const bcrypt = require("bcrypt");
 
 async function seedUsers() {
   try {
     await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "invoices" table if it doesn't exist
+    // Create the "users" table if it doesn't exist
     const createTable = await sql`
       CREATE TABLE IF NOT EXISTS users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -23,16 +23,27 @@ async function seedUsers() {
     console.log(`Created "users" table`);
 
     // Insert data into the "users" table
-    const insertedUsers = await Promise.all(
-      users.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        return sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-      }),
-    );
+    // const insertedUsers = await Promise.all(
+    //   users.map(async (user) => {
+    //     const hashedPassword = await bcrypt.hash(user.password, 10);
+    //     return sql`
+    //     INSERT INTO users (id, name, email, password)
+    //     VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+    //     ON CONFLICT (id) DO NOTHING;
+    //   `;
+    //   }),
+    // );
+    const insertedUsers = [];
+    for (const user of users) {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      insertedUsers.push(
+        await sql`
+      INSERT INTO users (id, name, email, password)
+      VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+      ON CONFLICT (id) DO NOTHING;
+      `,
+      );
+    }
 
     console.log(`Seeded ${insertedUsers.length} users`);
 
@@ -41,7 +52,7 @@ async function seedUsers() {
       users: insertedUsers,
     };
   } catch (error) {
-    console.error('Error seeding users:', error);
+    console.error("Error seeding users:", error);
     throw error;
   }
 }
@@ -64,15 +75,25 @@ async function seedInvoices() {
     console.log(`Created "invoices" table`);
 
     // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
+    // const insertedInvoices = await Promise.all(
+    //   invoices.map(
+    //     (invoice) => sql`
+    //     INSERT INTO invoices (customer_id, amount, status, date)
+    //     VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+    //     ON CONFLICT (id) DO NOTHING;
+    //   `,
+    //   ),
+    // );
+    const insertedInvoices = [];
+    for (const invoice of invoices) {
+      insertedInvoices.push(
+        await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+      ON CONFLICT (id) DO NOTHING;
       `,
-      ),
-    );
+      );
+    }
 
     console.log(`Seeded ${insertedInvoices.length} invoices`);
 
@@ -81,7 +102,7 @@ async function seedInvoices() {
       invoices: insertedInvoices,
     };
   } catch (error) {
-    console.error('Error seeding invoices:', error);
+    console.error("Error seeding invoices:", error);
     throw error;
   }
 }
@@ -103,15 +124,25 @@ async function seedCustomers() {
     console.log(`Created "customers" table`);
 
     // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
+    // const insertedCustomers = await Promise.all(
+    //   customers.map(
+    //     (customer) => sql`
+    //     INSERT INTO customers (id, name, email, image_url)
+    //     VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+    //     ON CONFLICT (id) DO NOTHING;
+    //   `,
+    //   ),
+    // );
+    const insertedCustomers = [];
+    for (const customer of customers) {
+      insertedCustomers.push(
+        await sql`
+      INSERT INTO customers (id, name, email, image_url)
+      VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+      ON CONFLICT (id) DO NOTHING;
       `,
-      ),
-    );
+      );
+    }
 
     console.log(`Seeded ${insertedCustomers.length} customers`);
 
@@ -120,7 +151,7 @@ async function seedCustomers() {
       customers: insertedCustomers,
     };
   } catch (error) {
-    console.error('Error seeding customers:', error);
+    console.error("Error seeding customers:", error);
     throw error;
   }
 }
@@ -138,15 +169,25 @@ async function seedRevenue() {
     console.log(`Created "revenue" table`);
 
     // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
+    // const insertedRevenue = await Promise.all(
+    //   revenue.map(
+    //     (rev) => sql`
+    //     INSERT INTO revenue (month, revenue)
+    //     VALUES (${rev.month}, ${rev.revenue})
+    //     ON CONFLICT (month) DO NOTHING;
+    //   `,
+    //   ),
+    // );
+    const insertedRevenue = [];
+    for (const rev of revenue) {
+      insertedRevenue.push(
+        await sql`
+      INSERT INTO revenue (month, revenue)
+      VALUES (${rev.month}, ${rev.revenue})
+      ON CONFLICT (month) DO NOTHING;
       `,
-      ),
-    );
+      );
+    }
 
     console.log(`Seeded ${insertedRevenue.length} revenue`);
 
@@ -155,7 +196,7 @@ async function seedRevenue() {
       revenue: insertedRevenue,
     };
   } catch (error) {
-    console.error('Error seeding revenue:', error);
+    console.error("Error seeding revenue:", error);
     throw error;
   }
 }
